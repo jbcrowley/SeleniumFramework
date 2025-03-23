4a71088a8d3244f90da5eff871ad37367b4f97ef
Jeff Crowley <jbcrowley@hotmail.com>
2025-03-23 17:39:38 -0500

    Changed the ITestData interface and CSV related test data files

--- SeleniumFramework/TestData/CsvTestData.cs
+++ SeleniumFramework/TestData/CsvTestData.cs
@@ -1,21 +1,9 @@
-﻿using CsvHelper;
-using System.Globalization;
-
-namespace SeleniumFramework.TestData
+﻿namespace SeleniumFramework.TestData
 {
-    public class CsvTestData : ITestData
+    class CsvTestData
     {
-        /// <summary>
-        /// Returns a collection of data from a CSV file for the current test
-        /// </summary>
-        /// <returns>The data for the current test</returns>
-        public Dictionary<string, string> GetTestData()
-        {
-            using (StreamReader reader = new StreamReader(new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory) + @"\TestData\TestData.csv"))
-            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                return csv.GetRecords<TestData>().Where(d => d.TestName == TestContext.CurrentContext.Test.Name).ToDictionary(x => x.Property, y => y.Value);
-            }
-        }
+        public string TestName { get; set; }
+        public string Property { get; set; }
+        public string Value { get; set; }
     }
 }
---
+++ SeleniumFramework/TestData/CsvTestDataHandler.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace SeleniumFramework.TestData
+{
+    public class CsvTestDataHandler : ITestData
+    {
+        /// <summary>
+        /// Returns a collection of data from a CSV file for the current test
+        /// </summary>
+        /// <returns>The data for the current test</returns>
+        public Dictionary<string, string> GetTestData()
+        {
+            using (StreamReader reader = new StreamReader(new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory) + @"\TestData\TestData.csv"))
+            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<CsvTestData>().Where(d => d.TestName == TestContext.CurrentContext.Test.Name).ToDictionary(x => x.Property, y => y.Value);
+            }
+        }
+    }
+}
