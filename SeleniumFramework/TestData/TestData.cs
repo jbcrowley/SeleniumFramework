6018a584e68a25b4e8a6d92d988a599090a663f3
Jeff Crowley <jbcrowley@hotmail.com>
2025-03-23 17:43:27 -0500

    Removed unnecessary file

--- SeleniumFramework/TestData/TestData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SeleniumFramework.TestData
-{
-    class TestData
-    {
-        public string TestName { get; set; }
-        public string Property { get; set; }
-        public string Value { get; set; }
-    }
-}
